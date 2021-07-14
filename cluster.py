92bdd25a7ef05b756710c364fb0c3cd0e735f441
Sam Bruce <sam@Sams-MBP-2.fios-router.home>
2021-07-14 08:15:08 -0400

    v2

--- cluster.py
+++ cluster.py
@@ -18,18 +18,26 @@ def get_type(name):
         return 'src'
     if 'fab' in n:
         return 'fab'
+    if 'step' in n:
+        return 'step'
+    return None
+
+def get_extension(name):
+    if '.' in name:
+        return name[name.index('.'):].lower()
     return None
 
 class Node:
     def __init__(self,name):
         self.name = name
         self.type = get_type(name)
+        self.extension = get_extension(name)
         self.children = []
     
     def add_child(self,child):
         self.children.append(child)
     
-    def get_children(self):
+    def __iter__(self):
         yield from self.children
     
     def get_all_children_names(self):
@@ -72,23 +80,99 @@ for file in filenames:
             else:
                 parent.add_child(new_file)
 
+# for file in directory:
+#     valid_count = 0
+#     for child in file:
+#         if child.type:
+#             valid_count+=1
+#     if valid_count >1:
+#         child.type = 'cca'
 
+'''
+.xlsx/.xls = BOM
+.docx = Rework
+.gwk = Unknown
+.jpg = rework/mod/installation/drawing
+.ppt = user guide
+.pptx = rework/conversion
+.max = unknown
+.sch = schematic
+.zip = fab/source/cam/unnamed
+.dsn = unknown
+.pcb = unknown
+.bmp = unknown
+.brd = unknown
+.pdf = schematic/assembly/bom
+.csv = bom (deeper is pick place output)
+.step = step
+.ipc = unknown
+.txt = random
+.rep = unknown
+'''
 
 print(directory)
-for child in directory.get_children():
-    not_printed = True
-    if not child.type:
-        not_printed = False
+# extensions = set()
+# for child in directory:
+    # not_printed = True
+    # if not child.type:
+    #     not_printed = False
         # print('\t'+ str(child))
     # print(child.children)
-    for grandchild in child.get_children():
-        if not grandchild.type:
-            # if not_printed: 
-            #     not_printed = False
-            #     # print('\t'+ str(child))
-            if str(grandchild)[len(str(grandchild))-4:len(str(grandchild))] == '.pdf':
-                print('\t\t'+ str(grandchild))
- 
+    # for grandchild in child:
+    #     extensions |= {grandchild.extension}
+    #     # if not grandchild.type:
+    #         # if not_printed: 
+    #         #     not_printed = False
+    #         #     # print('\t'+ str(child))
+    #         # if str(grandchild)[len(str(grandchild))-4:len(str(grandchild))] == '.pdf':
+    #         #     print('\t\t'+ str(grandchild))
+    #     if not grandchild.extension  and not grandchild.type:
+    #         # print('\t\t'+ str(grandchild))
+
+acceptable_chars = {'1','2','3','4','5','6','7','8','9','0','X','-'}
+feature_vectors = {}
+
+def get_pn(name):
+    pn = None
+    i=0
+    while not pn and i <= len(name)-11:
+        substr = name[0+i:11+i]
+        valid = True
+        for char in substr:
+            if char not in acceptable_chars:
+                valid = False
+                break
+        if valid:
+            pn = name[0+i:11+i]
+        i+=1
+    return pn
+
+def get_rev(name):
+    rev = None
+    i = 0
+    while not rev and i <= len(name)-2:
+        substr = name[0+i:2+i]
+        if substr[0] in ('R','X') and substr[1].isalpha():
+            rev = substr
+        i+=1
+    return rev
+
+for child in directory:
+    vec = {'pn': get_pn(child.name),'node':child,'type':child.type,'ext':child.extension,'rev':get_rev(child.name)}
+    if child.name in feature_vectors:
+        print('DANGER1',child.name)
+    feature_vectors[child.name] = vec
+    for grandchild in child:
+        vec = {'pn': get_pn(grandchild.name),'node':grandchild,'type':grandchild.type,'ext':grandchild.extension,'rev':get_rev(grandchild.name)}
+    if grandchild.name in feature_vectors:
+        print('DANGER2',grandchild.name)
+    feature_vectors[grandchild.name] = vec
+print(feature_vectors['Source Documents'])
+        
+            
+
+# print(extensions)
+
 # for file in directory.get_all_children_names():
 #     if file[len(file)-4:len(file)] == '.pdf':
 #         print(file)
